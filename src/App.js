295033d3d68df656da588a31b750a843bf361b8f
Célio Júnior <celiojunior0110@gmail.com>
2022-04-28 21:20:10 -0300

    Configurações Globais

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { GlobalStyle } from './styles/GlobalStyle'
 
-function App() {
+import { ThemeProvider } from 'styled-components'
+import Theme from './styles/Theme'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <h1>App</h1>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
