8c07da1483be3da151dc3cef39ed64c25fa9d6c4
gabriel <dgabridb3@gmail.com>
2022-05-10 19:14:03 -0300

    ...

--- src/router/Coordinator.js
+++ src/router/Coordinator.js
@@ -2,29 +2,18 @@ export const goToLogin = (navigate) => {
   navigate("/");
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> e79461733eb049352b4e2a04308c475f40a6b046
+
 export const goToHome = navigate => {
   navigate('/home')
 }
 export const goToBuscar = navigate => {
   navigate('/home/busca')
 }
-<<<<<<< HEAD
-=======
-export const goToHome = (navigate) => {
-  navigate("/home");
-};
->>>>>>> dd8bbafb70f40e0870cf9c73aae58de263f25d69
-=======
->>>>>>> e79461733eb049352b4e2a04308c475f40a6b046
 
 export const goToRestaurant = (navigate, restaurantId) => {
   navigate(`/restaurante/${restaurantId}`);
 };
-
+ 
 export const goToCart = (navigate) => {
   navigate("/carrinho");
 };
