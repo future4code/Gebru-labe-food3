d0d8f1e9040ed90438fb8085b04cbc1198c589be
Your Name <you@example.com>
2022-05-11 17:37:10 -0400

    Styled-endereco-cadastro

--- src/pages/Login/Cadastro/Cadastro.js
+++ src/pages/Login/Cadastro/Cadastro.js
@@ -4,6 +4,9 @@ import { API } from "../../../services/Api"
 import { useNavigate } from "react-router-dom"
 import { goToEndereco } from "../../../router/Coordinator"
 import { goToLogin } from "../../../router/Coordinator"
+import logo from "../../../img/logo.png"
+import iconVoltar from "../../../img/iconVoltar.svg"
+import { Container , ContainerCadastro,ContainerButoonVoltar,ImgButtonVoltar,ButtonVolter,LogoPhoto,Title,TextTitler,ContainerInputNome ,LabelNome,InputNome,LabelEmail,ContainerInputEmail,InputEmail,LabelCpf,ContainerInputCpf ,InputCpf,LabelSenha,ContainerInputSenha,InputSenha,LabelConfirmaSenha,ContainerInputConfirmaSenha,InputConfirmaSenha,ContainerButton,ButtonCriar, } from "./StyledCadastro"
 
 
 
@@ -31,6 +34,7 @@ export const Cadastro = () =>{
         })
         .catch((error)=>{
             alert(`Erro`, error.response.data)
+            console.log(error.response.data)
         })
 
     }
@@ -38,71 +42,105 @@ export const Cadastro = () =>{
 
 
     return (
-        <div>
-            
-           <button onClick={()=>goToLogin(navigate)}>Voltar</button>
-            <h1>cadastro</h1>
+        <Container>
+            <ContainerCadastro> 
+                <ContainerButoonVoltar>
+                <ButtonVolter onClick={()=>goToLogin(navigate)}><ImgButtonVoltar src={iconVoltar}/></ButtonVolter>
+                </ContainerButoonVoltar>
 
-             <form onSubmit={submitCadastro}>
-                <input
-                    name={"name"}
-                    value={form.name}
-                    onChange={onChange}
-                    placeholder={"Nome e sobrenome"}
-                    required
-                    pattern={'^.{3,}'}
-                    title={'seu nome deve ter no mínimo 3 caracteres'}
-                    type={"name"}
-                />
-                <input
-                    name={"email"}
-                    value={form.email}
-                    onChange={onChange}
-                    placeholder={"email@email.com"}
-                    required
-                    type={"email"}
-                />
-                <input
-                    name={"cpf"}
-                    value={form.cpf}
-                    onChange={onChange}
-                    placeholder={"cpf 000.000.000.00"}
-                    required
-                    pattern={'^.{11,}'}
-                    title={'seu cpf deve ter no mínimo 11 numeros'}
-                    type={"Number"}
-                    maxLength={11}
+                <LogoPhoto src={logo} />
+                <Title > 
+                    <TextTitler>Cadastro</TextTitler>
+                </Title>
                 
-                />
-                <input
-                    name={"password"}
-                    value={form.password}
-                    onChange={onChange}
-                    placeholder={"Senha Minimo 6 caracteres"}
-                    required
-                    type={"password"}
-                    pattern={'^.{6,}'}
-                    title={'A senha deve ter no mínimo 6 caracteres'}
-                />
 
-                {/* <input
-                    name={"password"}
-                    value={confirmSenha}
-                    onChange={onConfirmeSenha}
-                    placeholder={"Confirme a senha anterior"}
-                    required
-                    type={"password"}
-                    pattern={'^.{6,}'}
-                    title={'A senha deve ter no mínimo 6 caracteres'}
-                /> */}
-                <button>Criar</button>
+                <form   onSubmit={submitCadastro}>
+                    
+                         <LabelNome>Nome*</LabelNome>
+                    
+                    
+                <ContainerInputNome >
+                    <InputNome
+                            name={"name"}
+                            value={form.name}
+                            onChange={onChange}
+                            placeholder={"    Nome e sobrenome"}
+                            required
+                            pattern={'^.{3,}'}
+                            title={'seu nome deve ter no mínimo 3 caracteres'}
+                            type={"name"}
+                        />   
+                </ContainerInputNome>  
+                   
+                    <LabelEmail>E-mail*</LabelEmail>
+                 <ContainerInputEmail>
+                    <InputEmail
+                            name={"email"}
+                            value={form.email}
+                            onChange={onChange}
+                            placeholder={"   email@email.com"}
+                            required
+                            type={"email"}
+                        />
+                </ContainerInputEmail>   
+                    
+                    <LabelCpf>CPF*</LabelCpf>
+                 <ContainerInputCpf >
+                    <InputCpf
+                            name={"cpf"}
+                            value={form.cpf}
+                            onChange={onChange}
+                            placeholder={"   cpf 000.000.000.00"}
+                            required
+                            pattern={'^.{11,}'}
+                            title={'   seu cpf deve ter no mínimo 11 numeros'}
+                            type={"Number"}
+                            maxLength={11}
+                        
+                        />
+                 </ContainerInputCpf>   
+                    
+                    <LabelSenha>Senha*</LabelSenha>
+                    <ContainerInputSenha>
+                        <InputSenha
+                            name={"password"}
+                            value={form.password}
+                            onChange={onChange}
+                            placeholder={"   Senha Minimo 6 caracteres"}
+                            required
+                            type={"password"}
+                            pattern={'^.{6,}'}
+                            title={'A senha deve ter no mínimo 6 caracteres'}
+                        />
+                    </ContainerInputSenha>
+                   
+                    {/* <LabelConfirmaSenha>Confirmar*</LabelConfirmaSenha> */}
+                    {/* <ContainerInputConfirmaSenha>
+                        <InputConfirmaSenha
+                            name={"password"}
+                            value={form.password}
+                            onChange={onChange}
+                            placeholder={"    Confirme a senha anterior"}
+                            required
+                            type={"password"}
+                            pattern={'^.{6,}'}
+                            title={'A senha deve ter no mínimo 6 caracteres'}
+                        />
+                    </ContainerInputConfirmaSenha> */}
+                    <ContainerButton> 
+                        <ButtonCriar>Criar</ButtonCriar>
+                    </ContainerButton>
+                   
 
-            </form>
+                  </form>
 
 
 
 
-        <button onClick={()=>goToEndereco(navigate)}> endereço </button> 
-        </div>
+                
+                
+            </ContainerCadastro>  
+          
+        </Container>
     )
 } 
---
+++ src/pages/Login/Cadastro/StyledCadastro.js
@@ -0,0 +1,263 @@
+import styled from "styled-components"
+
+
+export const Container = styled.div`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    
+
+`
+
+export const ContainerCadastro = styled.div `
+    display: flex;
+    flex-direction: column;
+    row-gap: 5px;
+     align-items:center;
+    width: 360px;
+    height: 640px;
+    border: 1px solid black;
+    padding-top:5px;
+    padding-bottom:20px;
+    
+
+`
+export const ContainerButoonVoltar = styled.div`
+    display: flex;
+    width: 360px;
+    height: 35px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+
+`
+export const ImgButtonVoltar = styled.img`
+  width: 25px;
+`
+export const ButtonVolter = styled.button`
+  width: 33px;
+  height: 27px;
+  margin-left:16px;
+  margin-top: 5px;
+  border: none;
+   
+`
+export const LogoPhoto = styled.img`
+  width: 104px;
+  height: 58px;  
+  margin-top: 10px;
+  padding-top:5px ;
+  padding-bottom: 5px;
+`
+
+export const Title = styled.div`
+    width: 360px;
+    height:4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 15px;
+    
+    
+`
+export const TextTitler =styled.h1`
+
+  width: 296px;
+  height: 18px;
+  font-family: Roboto;
+  font-size: 18px;
+  font-weight: normal;
+  text-align: center;
+
+`
+
+
+export const LabelNome =styled.label`
+    width: 78px;
+    height: 18px;
+    margin: 3px 234px 43px 16px;
+    padding-right:20px;
+    font-family: Roboto;
+    font-size: 12px;
+    letter-spacing: -0.29px;
+    color: #b8b8b8;
+    position: relative;
+    top: 17px;
+    left: 20px;
+    background-color: white;
+
+`
+export const ContainerInputNome = styled.div`
+
+    width: 360px;
+    height: 4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    
+    
+
+`
+export const InputNome = styled.input`
+     width: 328px;
+    height: 56px;
+    font-family: Roboto;
+    border: solid 1px  #b8b8b8;
+    margin-right: 0px;
+
+`
+
+export const LabelEmail =styled.label`
+    width: 78px;
+    height: 18px;
+    margin: 3px 234px 43px 16px;
+    padding-right:20px;
+    font-family: Roboto;
+    font-size: 12px;
+    letter-spacing: -0.29px;
+    color: #b8b8b8;
+    position: relative;
+    top: 17px;
+    left: 20px;
+    background-color: white;
+
+`
+export const ContainerInputEmail= styled.div`
+
+    width: 360px;
+    height: 4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+`
+export const InputEmail= styled.input`
+     width: 328px;
+    height: 56px;
+    font-family: Roboto;
+    border: solid 1px  #b8b8b8;
+    margin-right: 0px;
+
+`
+export const LabelCpf =styled.label`
+    width: 78px;
+    height: 18px;
+    margin: 3px 234px 43px 16px;
+    padding-right:20px;
+    font-family: Roboto;
+    font-size: 12px;
+    letter-spacing: -0.29px;
+    color: #b8b8b8;
+    position: relative;
+    top: 17px;
+    left: 20px;
+    background-color: white;
+
+`
+export const ContainerInputCpf = styled.div`
+
+    width: 360px;
+    height:4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+
+`
+export const InputCpf = styled.input`
+     width: 328px;
+    height: 56px;
+    font-family: Roboto;
+    border: solid 1px  #b8b8b8;
+    margin-right: 0px;
+
+`
+export const LabelSenha =styled.label`
+    width: 78px;
+    height: 18px;
+    margin: 3px 234px 43px 16px;
+    padding-right:20px;
+    font-family: Roboto;
+    font-size: 12px;
+    letter-spacing: -0.29px;
+    color: #b8b8b8;
+    position: relative;
+    top: 17px;
+    left: 20px;
+    background-color: white;
+
+`
+export const ContainerInputSenha = styled.div`
+
+    width: 360px;
+    height: 4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+
+`
+export const InputSenha = styled.input`
+     width: 328px;
+    height: 56px;
+    font-family: Roboto;
+    border: solid 1px  #b8b8b8;
+    margin-right: 0px;
+
+`
+export const LabelConfirmaSenha =styled.label`
+    width: 78px;
+    height: 18px;
+    margin: 3px 234px 43px 16px;
+    padding-right:20px;
+    font-family: Roboto;
+    font-size: 12px;
+    letter-spacing: -0.29px;
+    color: #b8b8b8;
+    position: relative;
+    top: 17px;
+    left: 20px;
+    background-color: white;
+
+`
+export const ContainerInputConfirmaSenha = styled.div`
+
+    width: 360px;
+    height: 4em;
+    display: flex;;
+    justify-content: center;
+    align-items: center;
+    
+
+`
+export const InputConfirmaSenha = styled.input`
+     width: 328px;
+    height: 56px;
+    font-family: Roboto;
+    border: solid 1px  #b8b8b8;
+    
+
+`
+export const ContainerButton = styled.div`
+    width: 360px;
+    height: 3em;
+    margin-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+`
+export const ButtonCriar = styled.button`
+    width: 296px;
+    height: 42px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 600;
+    text-align:center;
+    border: none;
+    background-color: #5cb646;
+    
+`
